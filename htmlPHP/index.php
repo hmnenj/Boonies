2cbde935a47db56629a9d48d17ec27bab3489dd7
Heloysa Assis <137557559+hmnenj@users.noreply.github.com>
2024-06-19 19:50:00 -0300

    first commit

---
+++ htmlPHP/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boonies</title>
+</head>
+<body>
+    <h1></h1>
+        <div>
+            <button><a href="./cadastroCliente.php">Quero comprar</a></button>
+        </div>
+        <div>
+            <button><a href="./cadastroVendedor">Quero Vender</a></button>
+        </div>
+
+    </form>
+</body>
+</html>
